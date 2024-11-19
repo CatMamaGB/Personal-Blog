1ea3700fe09a4f1656fb9a0f84c301d707efb870
CatMamaGB <165972971+CatMamaGB@users.noreply.github.com>
2024-11-18 22:38:16 -0600

    Update files styling and fixed links

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,3 @@
-// This file can contain shared utilities and functions
 const utils = {
     // Format date
     formatDate: (date) => {
@@ -9,16 +8,4 @@ const utils = {
     validateInput: (text) => {
         return text && text.trim().length > 0;
     },
-
-    // Sanitize HTML (basic implementation)
-    sanitizeHTML: (text) => {
-        const div = document.createElement('div');
-        div.textContent = text;
-        return div.innerHTML;
-    }
 };
-
-// Export utils if using modules
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = utils;
-} 
